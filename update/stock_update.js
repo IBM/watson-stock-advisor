38e5996a3e39ac46654a600c79dd7684508345d0
Charles Reitz <charles.g.reitz@ibm.com>
2017-12-11 13:49:53 -0800

    Add logic for updating article history in DB

--- update/stock_update.js
+++ update/stock_update.js
@@ -14,8 +14,11 @@ var cloudant = Cloudant({
 });
 var db = cloudant.db.use(DB_NAME);
 
+//TODO these should be the companies' tickers
+var companies = ['A', 'B', 'C', 'D'];
+
 function isFunc(func) {
-    return typeof func == "function";
+    return typeof func == 'function';
 }
 
 function getDocs(callback) {
@@ -24,24 +27,96 @@ function getDocs(callback) {
         include_docs: true
     }, function(err, data) {
         if (isFunc(callback)) {
-            callback(err, data.rows);
+            var docs = data.rows.map(function(row) {
+                return row.doc;
+            });
+            callback(err, docs);
         }
     });
 }
 
 //include _id and _rev of existing doc in the doc to perform an update
-function insertDoc(doc, callback) {
+function insertOrUpdateDoc(doc, callback) {
+
     db.insert(doc, function(err, body, header) {
         if (err) {
-            console.log('insertion failed: ', err.message);
+            console.log('insertion failed: ' + err.message);
         } else if (isFunc(callback)) {
             callback(body, header);
         }
     });
 }
 
-function updateStocks() {
+function findStockDatum(stocks, company) {
 
+    for (var i = 0; i < stocks.length; i++) {
+        var stock = stocks[i];
+        if (stock.ticker == company) {
+            return stock;
+        }
+    }
+
+    return undefined;
 }
 
-updateStocks();
+function updateStocksData(articleData, stockData) {
+
+    for (var i = 0; i < articleData.length; i++) {
+        var articleDatum = articleData[i];
+        var stockDatum = findStockDatum(stockData, articleDatum.company);
+
+        if (stockDatum) {
+            stockDatum.history.unshift(articleDatum.article);
+        } else {
+            stockDatum = {
+                ticker: articleDatum.company,
+                history: [articleDatum.article]
+            };
+        }
+
+        //TODO batch insert?
+        console.log('Inserting into company ' + articleDatum.company + ' article: ' );
+        console.log(articleDatum.article);
+        insertOrUpdateDoc(stockDatum);
+    }
+}
+
+function getArticleDataForCompanies(companies, callback) {
+
+    //TODO use Watson discovery API to retrieve articles based on companies
+    var articleData = [];
+    var sentiments = ['positive', 'negative', 'neutral'];
+    for (var i = 0; i < companies.length; i++) {
+        articleData.push({
+            article: {
+                url: 'http://example.not-real.com/article' + (i + 1),
+                sentiment: sentiments[i % sentiments.length],
+                date: new Date(),
+            },
+            company: companies[i]
+        });
+    }
+
+    if (isFunc(callback)) {
+        callback(undefined, articleData);
+    }
+}
+
+function run() {
+
+    getArticleDataForCompanies(companies, function(articlesErr, articleData) {
+        if (!articlesErr) {
+            getDocs(function(docsErr, stockData) {
+                if (!docsErr) {
+                    updateStocksData(articleData, stockData);
+                } else {
+                    console.log(docsErr);
+                }
+            });
+        } else {
+            console.log(articlesErr);
+        }
+    });
+}
+
+run();

f1fe262419beafea020a927c6beb3121527d2fb8
Grishma Jena <GJena@users.noreply.github.com>
2017-12-13 15:35:02 -0800

    Create get_docs.js
    Retrieve documents from Discovery News

---
+++ discovery/get_docs.js
@@ -0,0 +1,19 @@
+//---CONFIGURE AUTHENTICATION HERE--------------------
+var DISCOVERY_KEY      = 'XXXXXXXX';
+var DISCOVERY_PASSWORD = 'YYYYYYYY';
+var DISCOVERY_VERSION   = 'v1'
+var DISCOVERY_VERSION_DATE = '2017-11-07'
+var ENV_ID = 'system'
+//----------------------------------------------------
+var Discovery = require('watson-developer-cloud/discovery/v1');
+var discovery = new Discovery({
+   username: DISCOVERY_KEY,
+   password: DISCOVERY_PASSWORD,
+   version: DISCOVERY_VERSION,
+   version_date: DISCOVERY_VERSION_DATE
+ });
+ 
+//retrieving titles for 5 documents for 'IBM Watson'
+discovery.query({ environment_id: ENV_ID, collection_id: 'news-en', query: 'IBM Watson', count:5, return:'title' }, function(error, data) {
+  console.log(JSON.stringify(data, null, 2));
+});

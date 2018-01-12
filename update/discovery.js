53d6b4784cc10543aaf22ce897805dc1f5f24948
jayakrishna-duvvuri <jayakrishnaduvvuri@gmail.com>
2018-01-12 11:58:19 -0800

    Adding Env variables such as Auth Keys as a JSON and config files

--- update/discovery.js
+++ update/discovery.js
@@ -1,16 +1,11 @@
-//---CONFIGURE AUTHENTICATION HERE--------------------
-var DISCOVERY_KEY      = 'XXXXXXXX';
-var DISCOVERY_PASSWORD = 'YYYYYYYY';
-var DISCOVERY_VERSION   = 'v1'
-var DISCOVERY_VERSION_DATE = '2017-11-07'
-var ENV_ID = 'system'
-//----------------------------------------------------
+
+const config = require('./config.js');
 var Discovery = require('watson-developer-cloud/discovery/v1');
 var discovery = new Discovery({
-   username: DISCOVERY_KEY,
-   password: DISCOVERY_PASSWORD,
-   version: DISCOVERY_VERSION,
-   version_date: DISCOVERY_VERSION_DATE
+   username: config.DISCOVERY_KEY,
+   password: config.DISCOVERY_PASSWORD,
+   version: config.DISCOVERY_VERSION,
+   version_date: config.DISCOVERY_VERSION_DATE
 });
  
 var utils = require('./utils.js');
@@ -19,7 +14,7 @@ var utils = require('./utils.js');
 function query(topic) {
   
   var promise = new Promise(function(resolve, reject) {
-    discovery.query({ environment_id: ENV_ID, collection_id: 'news-en', query: topic, count:5 }, function(error, data) {
+    discovery.query({ environment_id: config.ENV_ID, collection_id: 'news-en', query: topic, count:5 }, function(error, data) {
         if (error) {
           reject(error);
         } else {

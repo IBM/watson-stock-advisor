e340c1eb2fd5638bb4b4e14d76aea44b92b57cb4
Charles Reitz <charles.g.reitz@ibm.com>
2018-01-17 09:36:23 -0800

    Use app root path dynamically to reliably find .env in config.js to enable testing files individually

--- web/config.js
+++ web/config.js
@@ -1,10 +1,12 @@
 
-var fs = require('fs');
-require('dotenv').config()
+const fs = require('fs');
+const appRoot = require('app-root-path');
+const env_path  = appRoot + '/.env'
+require('dotenv').config({path: env_path})
 
 var configured = false;
 
-if (fs.existsSync('.env')) {
+if (fs.existsSync(env_path)) {
   configured = true;
 } else {
   console.log(".env file not found")

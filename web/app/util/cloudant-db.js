2241935665e5928bed1038c60af472fc9868ce4f
Charles Reitz <charles.g.reitz@ibm.com>
2018-01-16 16:52:56 -0800

    Add utils, cloudant db to webapp

---
+++ web/app/util/cloudant-db.js
@@ -0,0 +1,44 @@
+const Cloudant = require('cloudant');
+const utils = require('./utils.js');
+require('../../config.js');
+
+const cloudant = Cloudant({
+  account  : process.env.CLOUDANT_ACCESS,
+  key      : process.env.CLOUDANT_KEY,
+  password : process.env.CLOUDANT_PASSWORD
+});
+const db = cloudant.db.use(process.env.DB_NAME);
+
+function DB() {
+
+  const self = this;
+
+  self.search = () => {
+    return new Promise((resolve, reject) => {
+      db.list({ include_docs: true }, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+      });
+    });
+  };
+
+  self.insertOrUpdate = (doc) => {
+    console.log('updating: ', doc);
+    return new Promise((resolve, reject) => {
+      db.insert(doc, function(err, body, header) {
+        if (err) {
+          console.log('insertion failed: ' + err.message);
+          reject(err);
+        } else if (utils.isFunc(callback)) {
+          resolve(body, header);
+        }
+      });
+    });
+  };
+
+}
+
+module.exports = new DB();
--- web/server.js
+++ web/server.js
@@ -5,23 +5,24 @@ var methodOverride = require('method-override');
 
 var port = process.env.PORT || 8080; // set our port
 
-// parse application/json 
-app.use(bodyParser.json()); 
+// parse application/json
+app.use(bodyParser.json());
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with the X-HTTP-Method-Override header in the request
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set static files location
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
 // pass application into routes
-require('./app/routes')(app); 
+require('./app/routes')(app);
+require('./app/util/cloudant-db');
 
 // start app
-app.listen(port);  
+app.listen(port);
 console.log('Watson Stock Server starting on port ' + port);
 exports = module.exports = app;

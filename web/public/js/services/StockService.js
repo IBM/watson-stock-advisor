fb0840081c3c5b622144e16be215a7af06afd1ba
Charles Reitz <charles.g.reitz@ibm.com>
2018-01-18 17:06:32 -0800

    Add getStocks method to StockService

--- web/public/js/services/StockService.js
+++ web/public/js/services/StockService.js
@@ -2,5 +2,16 @@ angular.module('MainModule').factory('StockService', ['$http', function($http) {
 
   var service = {};
 
+  service.getStocks = function() {
+    return new Promise((resolve, reject) => {
+        $http.get('/api/stocks').then((result) => {
+          resolve(result.data);
+        }).catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  }
+
   return service;
 }]);

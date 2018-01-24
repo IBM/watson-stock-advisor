df307662cdce2c2c0696370c0ed2e165dc15dedb
Charles Reitz <charles.g.reitz@ibm.com>
2018-01-24 10:24:15 -0800

    Update version of body-parser and express

--- web/app/routes.js
+++ web/app/routes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
 
   // frontend routes
   app.get('*', function(req, res) {
-    res.sendfile('./public/index.html');
+    res.sendFile('./public/index.html');
   });
 
 };

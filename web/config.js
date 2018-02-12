e39c67b88344cd4643e92d37633412199df8169c
Charles Reitz <charles.g.reitz@ibm.com>
2018-02-12 13:25:07 -0800

    Read list of companies from file in config

--- web/config.js
+++ web/config.js
@@ -19,6 +19,36 @@ const appRoot = require('app-root-path');
 const env_path  = appRoot + '/.env'
 require('dotenv').config({path: env_path})
 
+/**
+ * Reads the contents of the given fileName and tries to parse them into companies
+ * @param {string} fileName - The full (relative) path of the file
+ */
+function loadCompanies(fileName) {
+  var companies = [];
+  var lines = fs.readFileSync(fileName, 'utf8').split('\n');
+  for (var i=0; i<lines.length; i++) {
+    var line = lines[i];
+    if (line.trim() !== '') {
+      var components = line.split('\t');
+      companies.push({
+        ticker: components[0],
+        name: components[1]
+      })
+    }
+  }
+  return companies;
+}
+
+//load the companies from files
+const dataPath = './app/data/';
+const dataFiles = ['NASDAQ_mapping.txt', 'NYSE_mapping.txt'];
+var companies = [];
+
+for (var x=0; x<dataFiles.length; x++) {
+  var file = dataPath + dataFiles[x];
+  companies = companies.concat(loadCompanies(file));
+}
+
 var configured = false;
 
 if (fs.existsSync(env_path)) {
@@ -28,5 +58,6 @@ if (fs.existsSync(env_path)) {
 }
 
 module.exports = {
-  configured : configured
+  configured : configured,
+  companies  : companies
 }
